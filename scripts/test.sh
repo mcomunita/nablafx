0732d92278538043b6da7f93bd86d73693c4bfa5
mcomunita <34679334+mcomunita@users.noreply.github.com>
2025-02-08 13:08:47 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
+CUDA_VISIBLE_DEVICES=0 \
 python scripts/main.py test \
---config logs-scratch-02/multidrive-ffuzz/S4-TTF/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5/config.yaml \
---ckpt_path "logs-scratch-02/multidrive-ffuzz/S4-TTF/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5/nnlinafx-PARAM/p362csrv/checkpoints/last.ckpt" \
---trainer.logger.entity team-mcomunita-qmul \
---trainer.logger.project nnlinafx-PARAM \
---trainer.logger.save_dir logs-scratch-02/multidrive-ffuzz/TEST/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5--last \
---trainer.logger.name bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5--last \
+--config logs/multidrive-ffuzz/S4-TTF/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5/config.yaml \
+--ckpt_path "logs/multidrive-ffuzz/S4-TTF/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5/nnlinafx-PARAM/p362csrv/checkpoints/last.ckpt" \
+--trainer.logger.entity mcomunita \
+--trainer.logger.project nablafx \
+--trainer.logger.save_dir logs/multidrive-ffuzz/TEST/bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5 \
+--trainer.logger.name bb_S4-TTF-B8-S32-C16_lr.01_td5_fd5 \
 --trainer.logger.group Multidrive-FFuzz_TEST \
 --trainer.logger.tags "['Multidrive-FFuzz', 'TEST']" \
 --trainer.accelerator gpu \
